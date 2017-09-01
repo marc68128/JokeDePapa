c40e5142ce2287344e450a851ce55e4a82f1ad06
Marc Unterseh <marc.unterseh@i-datech.fr>
2017-09-01 15:10:46 +0200

    Initial commit

---
+++ App4/App4/Model/Joke.cs
@@ -0,0 +1,9 @@
+﻿namespace App4.Model
+{
+    public class Joke
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string Sentence { get; set; }
+    }
+}
